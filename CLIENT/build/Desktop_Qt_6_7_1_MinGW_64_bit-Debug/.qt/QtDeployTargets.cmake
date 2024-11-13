5a7eca64e52ef4176ba4c452c316deef91e97cda
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2024-11-13 13:54:29 +0100

    -- Created base project for the QtApp in CLIENT

---
+++ CLIENT/build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_CLIENT_FILE C:/Users/Oroitz/Documents/Datos/ProjetsLaPlateforme/gits/Year_2/CPPRT/CLIENT/build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/CLIENT.exe)
+set(__QT_DEPLOY_TARGET_CLIENT_RUNTIME_DLLS C:/Qt/6.7.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6Core.dll)
